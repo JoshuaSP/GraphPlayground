8938c86e90c3508ae52b891d750de47ba947696b
JoshuaSP <joshua.s.penman@gmail.com>
2015-01-17 17:31:30 -0800

    Can drag nodes

--- app/scripts/firstcanvas.js
+++ app/scripts/firstcanvas.js
@@ -4,44 +4,27 @@ function toAlpha(num) {
 
 var radius = 16
 
-
-function GraphNode (point,i) {
-	this.point = point;
-	this.edges = [];
-	this.selected = false;
-	this.labelText = toAlpha(i);
-	this.circ = new Path.Circle({
-			center: point,
-			radius: radius
-		});
-	this.label = new PointText({
-			point: point,
-			content: this.labelText
-		});
-	console.log(this.label.bounds.center)
-	console.log(this.circ.position)
-	this.label.point -= this.label.bounds.center - this.circ.position
-	var that = this;
-	this.circ.onClick = function (event) {
-		if (Key.isDown('command')) {
-			this.remove();
-			that.label.remove();
-			graphNodes[i]=null;
-		}
-		else {
-			that.selected = true;
-			this.style = {fillColor: 'yellow'};
-			graphNodes.forEach (function(node) {
-				console.log (node)
-				if (that !== node) {
-					node.selected = false
-					node.circ.style = {fillColor: 'salmon'};
-				}
-			});
-			console.log(graphNodes)
-		}
-	}
+function createGraphNode(point,i) {
+	var gn = new Group ({
+		position: point,
+		children: [
+			new Path.Circle({
+				center: point,
+				radius: radius,
+				fillColor: 'salmon',
+				name: 'circle'
+			}),
+			new PointText({
+				position: point,
+				content: toAlpha(i),
+				name: 'label'
+			})],
+		idx: i
+	});
+	gn.children['label'].point -= gn.children['label'].bounds.center - point;
+	return gn;
 }
+
 
 var graphNodes = []
 
@@ -52,23 +35,38 @@ project.currentStyle = {
 };
 
 
+var gn = selectedNode = null
 function onMouseDown(event) {
-	// Add a segment to the path at the position of the mouse:
-	if (!project.hitTest(event.point)) {
+	hitresult = project.hitTest(event.point)
+	gn = null
+	if (!hitresult) {
 		var i = 0
 		while (graphNodes[i]) {i++;}
-		var newNode = new GraphNode(event.point,i);
+		var newNode = createGraphNode(event.point,i);
 		if (i < graphNodes.length) {
 			graphNodes[i] = newNode;
 		}
 		else {
 			graphNodes.push(newNode);
 		}
-		// var circ = new Path.Circle({
-		// 	center: event.point,
-		// 	radius: 20
-		// });
-		// circ.onClick = function (event) {this.remove();}
+	} else if(['circle','label'].indexOf(hitresult.item.name) != -1) {
+		gn = hitresult.item.parent;
+		if (event.modifiers.command) {
+			graphNodes[gn.idx] = null;
+			gn.remove();
+		} else {
+			if(graphNodes[selectedNode]) {
+				graphNodes[selectedNode].children['circle'].fillColor = 'salmon';
+			}
+			selectedNode = gn.idx;
+			gn.children['circle'].fillColor = 'yellow';
+		}
+	}
+}
+
+function onMouseDrag(event) {
+	if (gn) {
+		gn.position += event.delta;
 	}
 }
 
