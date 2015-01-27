2f825a682e88691033d667fd93fa4a3d1600ef44
JoshuaSP <joshua.s.penman@gmail.com>
2015-01-27 01:00:28 -0800

    different stuff is broken

--- web.js
+++ web.js
@@ -2,11 +2,21 @@ var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
 var logger = require('morgan');
- 
-app.use(logger);
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+ var path = require('path')
 
-app.get('/', function (req, res) {
-	res.sendFile('index.html')
-})
+// app.use(logger);
+
+// app.use(express.static(path.join(__dirname, 'dist')));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+// app.listen(process.env.PORT || 5000);
+
+
+// var express = require('express');
+// var app = express();
+// var path = require('path');
+
+//app.use(express.static(__dirname)); // Current directory is root
+app.use(express.static(path.join(__dirname, 'dist'))); //  "public" off of current is root
+
+app.listen(process.env.PORT || 5000);
+console.log('Listening on port 80');
