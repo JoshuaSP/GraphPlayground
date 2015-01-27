226e86e8e932254c9c2e46288ec4c87f7bcc5ec1
JoshuaSP <joshua.s.penman@gmail.com>
2015-01-26 21:09:16 -0800

    More deploy changes

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
+var logger = require('morgan');
  
-app.use(express.logger('dev'));
+app.use(logger);
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
