1717e2c207da5bdeae1146f23457fa95a3012164
JoshuaSP <joshua.s.penman@gmail.com>
2015-01-27 11:59:31 -0800

    my god it's all ready to deploy

---
+++ web.js
@@ -0,0 +1,22 @@
+var gzippo = require('gzippo');
+var express = require('express');
+var app = express();
+var logger = require('morgan');
+ var path = require('path')
+
+// app.use(logger);
+
+// app.use(express.static(path.join(__dirname, 'dist')));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+// app.listen(process.env.PORT || 5000);
+
+
+// var express = require('express');
+// var app = express();
+// var path = require('path');
+
+//app.use(express.static(__dirname)); // Current directory is root
+app.use(express.static(path.join(__dirname, 'dist'))); //  "public" off of current is root
+
+app.listen(process.env.PORT || 5000);
+console.log('Listening on port 80');
