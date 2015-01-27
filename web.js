b2b60b60bf7b56336da28a219be704be20061107
JoshuaSP <joshua.s.penman@gmail.com>
2015-01-26 21:47:04 -0800

    messing with express

--- web.js
+++ web.js
@@ -6,3 +6,7 @@ var logger = require('morgan');
 app.use(logger);
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
+
+app.get('/', function (req, res) {
+	res.sendFile('index.html')
+})
